f346a2e0ed7643af178c7ba934e06235455a8ece
tombijaoui <bijaouitom@gmail.com>
2025-03-25 23:25:11 +0200

    adding comments

--- utils/param_utils.py
+++ utils/param_utils.py
@@ -11,10 +11,10 @@ def get_args(for_query_adaptive_cde=True):
 
     parser.add_argument("--dataset", type=str, required=True, help="BEIR dataset name.")
     parser.add_argument("--index", type=int, required=True, help="Index to check.")
-    parser.add_argument("--seed", type=int, required=True, help="The seed of the program.")
 
     if for_query_adaptive_cde:
         parser.add_argument("--hidden_dim", type=int, required=True, help="Hidden Dimension For the Model.")
         parser.add_argument("--epochs", type=int, required=True, help="Number of Epochs for Training Procedure.")
+        parser.add_argument("--seed", type=int, required=True, help="The seed of the program.")
 
     return parser.parse_args()
